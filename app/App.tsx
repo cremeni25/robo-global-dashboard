82be45c6e4e66598d797bd8600b9db347df53554
cremeni25 <andersonnavarro.si@gmail.com>
2026-01-15 17:04:54 -0300

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,22 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
 import EstadoCapital from "./screens/EstadoCapital";
+import Ofertas from "./screens/Ofertas";
+import Decisao from "./screens/Decisao";
+import Logs from "./screens/Logs";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <EstadoCapital />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Estado" component={EstadoCapital} />
+        <Tab.Screen name="Ofertas" component={Ofertas} />
+        <Tab.Screen name="Decisão" component={Decisao} />
+        <Tab.Screen name="Logs" component={Logs} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
